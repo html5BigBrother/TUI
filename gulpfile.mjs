e1698edb18d0774819ce1517e2864a9dc481fac5
sp0344 <admin@admindeMacBook-Pro.local>
2022-11-04 13:29:23 +0800

    feat: 接入gulp，任务化管理构建流程

---
+++ gulpfile.mjs
@@ -0,0 +1,5 @@
+import gulp from "gulp";
+import release from "./gulpfile/release.mjs";
+export * from "./gulpfile/release.mjs";
+
+export { release };
--- gulpfile/release.mjs
+++ gulpfile/release.mjs
@@ -2,13 +2,14 @@ import { resolve as pathResolve, dirname } from 'path'
 import { exec } from 'child_process'
 import fs from 'fs'
 import { fileURLToPath } from 'url'
+import gulp from 'gulp'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // 获取更新的pkg，返回pkgNames
 const getUpdatedPkg = function() {
   return new Promise((resolve, reject) => {
-    exec(`pnpm changeset status --output=./scripts/output.json`, {}, (error, stdout, stderr) => {
+    exec(`pnpm changeset status --output=./gulpfile/output.json`, {}, (error, stdout, stderr) => {
 
       if (error) {
         console.error(`exec error: ${error}`);
@@ -17,7 +18,7 @@ const getUpdatedPkg = function() {
 
       const changesetJson = JSON.parse(fs.readFileSync(pathResolve(__dirname, 'output.json'), 'utf-8'))
       const pkgNames = changesetJson?.releases.map((item) => item.name)
-      exec('rm -rf ./scripts/output.json')
+      exec('rm -rf ./gulpfile/output.json')
       resolve(pkgNames)
     })
   })
@@ -25,33 +26,49 @@ const getUpdatedPkg = function() {
 
 // 根据传入的package name，批量打包
 const multiplePack = (pkgNames) => {
+  console.log('pkgNames', pkgNames)
+  const promiseAllList = []
   pkgNames?.forEach((pkgName) => {
     // 异步调用pnpm --filter,分别执行命中pkg的build方法
-    exec(`pnpm --filter ${pkgName} run build`, {}, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      // console.log(`stderr: ${stderr}`);
+    const promiseAllItem = new Promise((resolve) => {
+      exec(`pnpm --filter ${pkgName} run build`, {}, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        // console.log(`stderr: ${stderr}`);
+        resolve(true)
+      })
     })
+    promiseAllList.push(promiseAllItem)
   })
+  return Promise.all(promiseAllList)
+}
+
+// 打包更新的pkg
+const packUpdatedPkg = async () => {
+  const pkgNames = await getUpdatedPkg()
+  await multiplePack(pkgNames)
 }
 
 // 消费changeset，发布npm包
-const costAndPublish = () => {
+const costAndPublish = (cb) => {
   exec('pnpm changeset version && pnpm changeset publish', {}, (error, stdout, stderr) => {
     if (error) {
-      console.error(error);
+      cb(error);
       return;
     }
     console.log(stdout);
     console.log(stderr);
+    cb()
   })
 }
 
-(async function start() {
-  const pkgNames = await getUpdatedPkg()
-  multiplePack(pkgNames)
-  costAndPublish()
-})()
+// (async function start() {
+//   const pkgNames = await getUpdatedPkg()
+//   multiplePack(pkgNames)
+//   costAndPublish()
+// })()
+
+export default gulp.series(packUpdatedPkg, costAndPublish)
